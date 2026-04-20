43d22372bee0efb7a4203b5fd2dff6ab1f6f85c0
Jessica Ariadne Murphy <jessica.ariadne.murphy@GMail.com>
2026-04-20 12:25:47 -0400

    Hi

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -28,5 +28,3 @@ if (Test-Path($ChocolateyProfile)) {
 # ============================================================
 # Profile Loaded Successfully
 # ============================================================
-Functions
-Aliases
